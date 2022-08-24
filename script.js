2831f3be2ef34376fb22c36410d5baf8b99956f5
kubeshauseli17 <kubeshauseli17@gmail.com>
2022-08-24 14:03:55 -0700

    update

--- script.js
+++ script.js
@@ -71,212 +71,254 @@ function removeShip() {
     // playerSquares.push(sq);
 }
 //not allowed ship index placement
-,
+
 //validation function with parameters idx of clicked and size of selected ship
 //if/else-if statement for conditions (selectedShip size & orientation)
-//variable for which indexes are not o, kay per size
+//variable for which indexes are not okay per size
+function compareSquares(arr1, arr2) {
+    let multiMatches = [];
+    console.log(arr1);
 
-function validateGridSelection(idx, shipSize) {
-
-    if (shipSize = 2 && shipOrientation == false) {
-        const notAllowedVertical = [90, 91, 92, 93, 94, 95, 96, 97, 98, 99]
-
-    } elseif(shipSize = 2 && shipOrientation == true) {
-        const notAllowedHorizontal = [9, 19, 29, 39, 49, 59, 69, 79, 89, 99]
-
-    } elseif(shipSize > 2 && shipOrientation == false) {
-        const notAllowedVertical = [80, 90, 81, 91, 82, 92, 83, 93, 84, 94,
-                                    85, 95, 86, 96, 87, 97, 88, 98, 89, 99]
-
-    } elseif(shipSize > 2 && shipOrientation == true) {
-        const notAllowedHorizontal = [8, 9, 18, 19, 28, 29, 38, 39, 48, 49,
-                                      58, 59, 68, 69, 78, 79, 88, 89, 98, 99]
-
-    } elseif(shipSize > 3 && shipOrientation == false) {
-        const notAllowedVertical = [70, 80, 90, 71, 81, 91, 72, 82, 92, 73,
-                                    83, 93, 74, 84, 94, 75, 85, 95, 76, 86,
-                                    96, 77, 87, 97, 78, 88, 98, 79, 89, 99]
-
-    } elseif(shipSize > 3 && shipOrientation == true) {
-        const notAllowedHorizontal = [7, 8, 9, 17, 18, 19, 27, 28, 29, 37, 38,
-                                      39, 47, 48, 49, 57, 58, 59, 67, 68, 69,
-                                      77, 78, 79, 87, 88, 89, 97, 98, 99]
-
-    } elseif(shipSize > 4 && shipOrientation == false) {
-        const notAllowedVertical = [60, 70, 80, 90, 61, 71, 81, 91, 62, 72,
-                                    82, 92, 63, 73, 83, 93, 64, 74, 84, 94,
-                                    65, 75, 85, 95, 66, 76, 86, 96, 67, 77,
-                                    87, 97, 68, 78, 88, 98, 69, 79, 89, 99];
-
-    } elseif(shipSize > 4 && shipOrientation == true) {
-        const notAllowedHorizontal = [6, 7, 8, 9, 16, 17, 18, 19, 26, 27, 28,
-                                      29, 36, 37, 38, 39, 46, 47, 48, 49, 56,
-                                      57, 58, 59, 66, 67, 68, 69, 76, 77, 78,
-                                      79, 86, 87, 88, 89, 96, 97, 98, 99]
+    //loop through arr2
+    //if arr1[i].length > 1
+    //loop through arr1 and push matches into new array
+    //if new array == arr1[i] return true (matches)
+    for (let a = 0; a < arr1.length; a++) {
+        console.log(arr1[a]);
+        if (arr1[a] && arr1[a].length > 1) {
+            arr1[a].forEach(function (num) {
+                for (let b = 0; b < arr2.length; b++) {
+                    if (num == arr2[b]) {
+                        multiMatches.push(arr2[b])
+                    }
+                }
+            })
+            console.log(multiMatches, arr1[a]);
+            if (JSON.stringify(arr1[a]) == JSON.stringify(multiMatches)) {
+                return true;
+            } else {
+                matches = false;
+            }
+        } else {
+            for (let j = 0; j < arr2.length; j++) {
+                if (arr2[j] === arr1[a]) {
+                    return true;
+                }
+                else {
+                    matches = false;
+                }
+            }
+        }
     }
+    return matches
 }
 
-    function playerShipPlacement(clicked, idx) {
-        if (selectedShip) {
-            for (i = 0; i < selectedShip.size; i++) {
-                if (!shipOrientation) {
-                    currentIdx = idx + (10 * i)
-                    document.querySelectorAll('.pGridSquare').forEach(function (sq, index) {
-                        if (index == currentIdx) {
-                            sq.classList.add('player-square')
-                        }
-                    });
+function validateGridSelection(indexes, shipSize) {
 
-                } else {
-                    currentIdx = idx + (1 * i)
-                    document.querySelectorAll('.pGridSquare').forEach(function (sq, index) {
-                        if (index == currentIdx) {
-                            sq.classList.add('player-square')
-                        }
-                    });
+    if (shipSize == 2 && shipOrientation == false) {
+        const notAllowedVertical = [100, 101, 102, 103, 104, 105, 106, 107, 108, 109]
+        return compareSquares(notAllowedVertical, indexes)
 
-                }
+    } else if (shipSize == 2 && shipOrientation == true) {
+        const notAllowedHorizontal = [[9, 10], [19, 20], [29, 30], [39, 40], [49, 50],
+        [59, 60], [69, 70], [79, 80], [89, 90], [99, 100]]
+        return compareSquares(notAllowedHorizontal, indexes)
 
+    } else if (shipSize == 3 && shipOrientation == false) {
+        const notAllowedVertical = [100, 101, 102, 103, 104, 105, 106, 107, 108, 109]
+        return compareSquares(notAllowedVertical, indexes)
+
+    } else if (shipSize == 3 && shipOrientation == true) {
+        const notAllowedHorizontal = [[9, 10], [19, 20], [29, 30], [39, 40], [49, 50],
+        [59, 60], [69, 70], [79, 80], [89, 90], [99, 100]]
+        return compareSquares(notAllowedHorizontal, indexes)
+
+    } else if (shipSize == 4 && shipOrientation == false) {
+        const notAllowedVertical = [100, 101, 102, 103, 104, 105, 106, 107, 108, 109]
+        return compareSquares(notAllowedVertical, indexes)
+
+    } else if (shipSize == 4 && shipOrientation == true) {
+        const notAllowedHorizontal = [[9, 10], [19, 20], [29, 30], [39, 40], [49, 50],
+        [59, 60], [69, 70], [79, 80], [89, 90], [99, 100]]
+        return compareSquares(notAllowedHorizontal, indexes)
+
+    } else if (shipSize == 5 && shipOrientation == false) {
+        const notAllowedVertical = [100, 101, 102, 103, 104, 105, 106, 107, 108, 109];
+        return compareSquares(notAllowedVertical, indexes)
+
+    } else if (shipSize == 5 && shipOrientation == true) {
+        const notAllowedHorizontal = [[9, 10], [19, 20], [29, 30], [39, 40], [49, 50],
+        [59, 60], [69, 70], [79, 80], [89, 90], [99, 100]]
+        return compareSquares(notAllowedHorizontal, indexes)
+    }
+
+    //return true if can complete move false if cant
+}
+
+function playerShipPlacement(clicked, idx) {
+    let indexes = []
+    //set up indecies to be coloured
+    if (selectedShip) {
+        for (i = 0; i < selectedShip.size; i++) {
+            if (!shipOrientation) {
+                currentIdx = idx + (10 * i)
+                // console.log(currentIdx);
+                indexes.push(currentIdx)
+                // console.log(indexes);
+            } else {
+                currentIdx = idx + (1 * i)
+                indexes.push(currentIdx)
             }
-            removeShip();
-            selectedShip = null
+        }
+        //validate and colour, or dont
+        if (!validateGridSelection(indexes, selectedShip.size)) {
+            document.querySelectorAll('.pGridSquare').forEach(function (sq, index) {
+                indexes.forEach(function (idx) {
+                    if (index == idx) {
+                        sq.classList.add('player-square')
+                    }
+                })
+            });
         } else {
             return;
         }
+        removeShip();
+        selectedShip = null
+    } else {
+        return;
+    }
+
+}
+
+
+
+
+
+
+///create boards
+const makeBoards = () => {
+    for (let i = 0; i < 100; i++) {
+
+        const pGridSquare = document.createElement('div')
+        pGridSquare.classList.add('pGridSquare')
+        document.querySelector('.pboard').appendChild(pGridSquare)
+        //pGridSquare.classList.add('player-square')
+        //playerSquares.push(pGridSquare)
+
+
+    }
+
+    for (let c = 0; c < 100; c++) {
+        const cGridSquare = document.createElement('div')
+        cGridSquare.classList.add('cGridSquare')
+        document.querySelector('.cboard').appendChild(cGridSquare)
 
     }
 
 
 
+}
+
+// computer selection function. generate's ships and randomly places them.
+
+// function computerShipPlacement() {
+
+// computerShips = shipsArray.slice()
+
+// while(computerShips.length > 0) {
+//     const cShip = computerShips[0]
+//     cShipLength = cShip.size
+
+// let placeShip = true
+
+// // random number to choose ship orientation
+
+// const randomOrientation = Math.random() >= 0.5
+// randomIndex = Math.floor(Math.random() * cGridSquare.length)
+
+// let columnIndex = (randomIndex % width)
+// let rowIndex = Math.floor(randomIndex / width)
+
+// //Vertical ship
+// if (randomDirection === true) {
+//     direction = 10
+//     while ((rowIndex - 1 + cShipLength) >= width) {
+//         randomIndex = Math.floor(Math.random() * cGridSquare.length)
+//         columnIndex = (randomIndex % width)
+//         rowIndex = Math.floor(randomIndex / width)
+//     }
+// // horizonal
+// } else {
+//     direction = 1
+//     while ((width - columnIndex) < cShipLength) {
+//         randomIndex = Math.floor(Math.random() * cGridSquare.length)
+//         columnIndex = (randomIndex % width)
+//       }
+// }
+// }
+// }
+// // create computer ships
+// for (let i = 0; i < shipLength; i++) {
+//     const nextIndex = randomIndex + i * direction
+// }
 
 
+//create a function to loop through board squares on click.
+//for vertical add 10 for the next square. try to select next square by index.
 
-    ///create boards
-    const makeBoards = () => {
-        for (let i = 0; i < 100; i++) {
+makeBoards();
 
-            const pGridSquare = document.createElement('div')
-            pGridSquare.classList.add('pGridSquare')
-            document.querySelector('.pboard').appendChild(pGridSquare)
-            //pGridSquare.classList.add('player-square')
-            //playerSquares.push(pGridSquare)
+function startGame() {
 
+    //on start remove all disabled classes. disable start game button
+    document.querySelectorAll('.disabled').forEach(function (el) {
+        el.classList.remove("disabled");
+    })
 
-        }
-
-        for (let c = 0; c < 100; c++) {
-            const cGridSquare = document.createElement('div')
-            cGridSquare.classList.add('cGridSquare')
-            document.querySelector('.cboard').appendChild(cGridSquare)
-
-        }
-
-
-
-    }
-
-    // computer selection function. generate's ships and randomly places them.
-
-    // function computerShipPlacement() {
-
-    // computerShips = shipsArray.slice()
-
-    // while(computerShips.length > 0) {
-    //     const cShip = computerShips[0]
-    //     cShipLength = cShip.size
-
-    // let placeShip = true
-
-    // // random number to choose ship orientation
-
-    // const randomOrientation = Math.random() >= 0.5
-    // randomIndex = Math.floor(Math.random() * cGridSquare.length)
-
-    // let columnIndex = (randomIndex % width)
-    // let rowIndex = Math.floor(randomIndex / width)
-
-    // //Vertical ship
-    // if (randomDirection === true) {
-    //     direction = 10
-    //     while ((rowIndex - 1 + cShipLength) >= width) {
-    //         randomIndex = Math.floor(Math.random() * cGridSquare.length)
-    //         columnIndex = (randomIndex % width)
-    //         rowIndex = Math.floor(randomIndex / width)
-    //     }
-    // // horizonal
-    // } else {
-    //     direction = 1
-    //     while ((width - columnIndex) < cShipLength) {
-    //         randomIndex = Math.floor(Math.random() * cGridSquare.length)
-    //         columnIndex = (randomIndex % width)
-    //       }
-    // }
-    // }
-    // }
-    // // create computer ships
-    // for (let i = 0; i < shipLength; i++) {
-    //     const nextIndex = randomIndex + i * direction
-    // }
-
-
-    //create a function to loop through board squares on click.
-    //for vertical add 10 for the next square. try to select next square by index.
-
-    makeBoards();
-
-    function startGame() {
-
-        //on start remove all disabled classes. disable start game button
-        document.querySelectorAll('.disabled').forEach(function (el) {
-            el.classList.remove("disabled");
+    document.querySelectorAll('.pGridSquare').forEach(function (sq) {
+        sq.addEventListener('mouseenter', function () {
         })
 
-        document.querySelectorAll('.pGridSquare').forEach(function (sq) {
-            sq.addEventListener('mouseenter', function () {
-                console.log(selectedShip)
-            })
+    })
+
+    document.querySelectorAll('.pGridSquare').forEach(function (sq, index) {
+        sq.addEventListener('click', function () {
+            if (selectedShip) {
+                playerShipPlacement(sq, index);
+            }
 
         })
-
-        document.querySelectorAll('.pGridSquare').forEach(function (sq, index) {
-            sq.addEventListener('click', function () {
-                if (selectedShip) {
-                    sq.classList.add('player-square')
-                    playerShipPlacement(sq, index);
-                }
-
-            })
-        })
+    })
 
 
-        //promp player to select ships
+    //promp player to select ships
 
 
-        //generate computer ship positions
+    //generate computer ship positions
 
 
-    }
+}
 
-    //after - hover event, squares and click
+//after - hover event, squares and click
 
-    // render players ships on button click. mouse over diplay the cell 
-    // placement of the ship. append the ship there on mouseenter.
+// render players ships on button click. mouse over diplay the cell 
+// placement of the ship. append the ship there on mouseenter.
 
 
-    //more game buttons
+//more game buttons
 
-    const startButton = document.getElementById('startButton')
-    startButton.addEventListener('click', startGame)
+const startButton = document.getElementById('startButton')
+startButton.addEventListener('click', startGame)
 
-    const resetButton = document.getElementById('resetButton')
-    resetButton.addEventListener('click', function () {
-        console.log("click");
-    });
+const resetButton = document.getElementById('resetButton')
+resetButton.addEventListener('click', function () {
+    console.log("click");
+});
 
-    const rotateButton = document.getElementById('rotateShip')
-    rotateButton.addEventListener('click', function () {
-        shipOrientation = !shipOrientation
-    });
+const rotateButton = document.getElementById('rotateShip')
+rotateButton.addEventListener('click', function () {
+    shipOrientation = !shipOrientation
+});
 
 
 
